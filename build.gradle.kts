1d50730f25cdfdd9bafeb819de6bdd4468c2792e
Archy-X <63976867+Archy-X@users.noreply.github.com>
2024-06-19 16:16:53 -0700

    Add maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     `java-library`
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
